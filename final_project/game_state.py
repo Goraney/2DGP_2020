c45dd9ce9dd9c8d5da931c352cecc6b2b8fbca97
Goraney <btok92@gmail.com>
2020-11-19 00:01:30 +0900

    기본 state 준비

---
+++ final_project/game_state.py
@@ -0,0 +1,34 @@
+from pico2d import *
+import gfw
+from player import Player
+
+canvas_width = 800
+canvas_height = 600
+
+def enter():
+    gfw.world.init(['player'])
+
+    global player
+    player = Player()
+    gfw.world.add(gfw.layer.player, player)
+
+def exit():
+    pass
+
+def update():
+    gfw.world.update()
+
+def draw():
+    gfw.world.draw()
+
+def handle_event(e):
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    if e.type == SDL_KEYDOWN:
+        if e.key == SDLK_ESCAPE:
+            gfw.pop()
+
+    player.handle_event(e)
+
+if __name__ == '__main__':
+    gfw.run_main()

08eabf0f676f1b2b45b4876c5715f99a592517c5
Goraney <btok92@gmail.com>
2020-11-22 22:27:32 +0900

    오류 수정, 캐릭터 기본공격 추가

--- final_project/player.py
+++ final_project/player.py
@@ -28,6 +28,12 @@ class IdleState:
         self.time += gfw.delta_time
         dx, dy = self.player.delta
 
+        if dx != 0:
+            if dx < 0:
+                self.player.dir = -1
+            elif dx > 0:
+                self.player.dir = 1
+
         x, y = self.player.pos
         x += dx * self.player.speed_move * gfw.delta_time
         y += dy * self.player.speed_move * gfw.delta_time
@@ -53,21 +59,96 @@ class IdleState:
 
     def handle_event(self, e):
         pair = (e.type, e.key)
-        if pair in Player.KEY_MAP:
-            self.player.updateDelta(*Player.KEY_MAP[pair])
+        if pair in Player.KEYDOWN_MAP:
+            self.player.updateDelta(*Player.KEYDOWN_MAP[pair])
+        elif pair in Player.KEYUP_MAP:
+            self.player.updateDelta(*Player.KEYUP_MAP[pair])
+        elif pair == Player.KEYDOWN_z:
+            self.player.set_state(AttackState)
+
+class AttackState:
+    @staticmethod
+    def get(player):
+        if not hasattr(AttackState, 'singleton'):
+            AttackState.singleton = AttackState()
+            AttackState.singleton.player = player
+        return AttackState.singleton
+
+    def __init__(self):
+        self.image_attack = gfw.image.load('res/char_01_atk.png')
+
+    def enter(self):
+        self.time = 0
+        self.fidx = 0
+        self.comboswitch = 0
+
+        if self.player.dir < 0:
+            self.player.action_atk = 3
+        else:
+            self.player.action_atk = 7
+
+        self.player.action_atk -= self.player.combo % 4
+
+    def exit(self):
+        pass
+
+    def draw(self):
+        x = self.fidx * 128
+        y = self.player.action_atk * 128
+        self.image_attack.clip_draw(x, y, 128, 128, *self.player.pos)
+
+    def update(self):
+        self.time += gfw.delta_time
+        frame = self.time * 15
+
+        if frame < 6:
+            self.fidx = int(frame)
+        else:
+            self.player.combo += 1
+            if self.comboswitch == 0:
+                self.player.set_state(IdleState)
+            else:
+                self.player.set_state(AttackState)
+
+    def updateDelta(self, ddx, ddy):
+        dx, dy = self.player.delta
+        dx += ddx
+        dy += ddy
+        if ddx != 0:
+            self.player.updateAction(dx, ddx)
+        self.player.delta = dx, dy
+
+    def updateAction(self, dx, ddx):
+        self.player.action = \
+            3 if dx < 0 else \
+            4 if dx > 0 else \
+            5 if ddx > 0 else 6
+
+    def handle_event(self,e):
+        pair = (e.type, e.key)
+        if pair in Player.KEYDOWN_MAP:
+            self.player.updateDelta(*Player.KEYDOWN_MAP[pair])
+        elif pair in Player.KEYUP_MAP:
+            self.player.updateDelta(*Player.KEYUP_MAP[pair])
+        elif pair == Player.KEYDOWN_z:
+            self.comboswitch += 1
 
 
 class Player:
-    KEY_MAP = {
+    KEYDOWN_MAP = {
         (SDL_KEYDOWN, SDLK_LEFT):   (-1,  0),
         (SDL_KEYDOWN, SDLK_RIGHT):  ( 1,  0),
         (SDL_KEYDOWN, SDLK_DOWN):   ( 0, -1),
         (SDL_KEYDOWN, SDLK_UP):     ( 0,  1),
+    }
+    KEYUP_MAP = {
         (SDL_KEYUP, SDLK_LEFT):     ( 1,  0),
         (SDL_KEYUP, SDLK_RIGHT):    (-1,  0),
         (SDL_KEYUP, SDLK_DOWN):     ( 0,  1),
         (SDL_KEYUP, SDLK_UP):       ( 0, -1),
     }
+    KEYDOWN_z = (SDL_KEYDOWN, SDLK_z)
+    KEYDOWN_x = (SDL_KEYDOWN, SDLK_x)
     image = None
 
     def __init__(self):
@@ -79,6 +160,9 @@ class Player:
         self.time = 0
         self.fidx = 0
         self.action = 6
+        self.action_atk = 7
+        self.combo = 0
+        self.dir = 0
         self.delta = 0, 0
         self.state = None
         self.set_state(IdleState)

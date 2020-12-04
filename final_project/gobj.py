0b906d38c443e0feec0ee463e09647cb4ead112a
Goraney <btok92@gmail.com>
2020-12-05 00:05:42 +0900

    충돌체크/바운딩박스

--- final_project/gobj.py
+++ final_project/gobj.py
@@ -2,28 +2,28 @@ from pico2d import *
 import gfw
 
 def point_add(point1, point2):
-    x1,y1 = point1
-    x2,y2 = point2
+    x1, y1 = point1
+    x2, y2 = point2
     return x1+x2, y1+y2
 
 def collides_box(a, b):
-	(la, ba, ra, ta) = a.get_bb()
-	(lb, bb, rb, tb) = b.get_bb()
+    (la, ba, ra, ta) = a.get_bb()
+    (lb, bb, rb, tb) = b.get_bb()
 
-	if la > rb: return False
-	if ra < lb: return False
-	if ba > tb: return False
-	if ta < bb: return False
+    if la > rb: return False
+    if ra < lb: return False
+    if ba > tb: return False
+    if ta < bb: return False
 
-	return True
+    return True
 
 def distance_sq(point1, point2):
-    x1,y1 = point1
-    x2,y2 = point2
-    return (x1-x2)**2 + (y1-y2)**2
+    x1, y1 = point1
+    x2, y2 = point2
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 def distance(point1, point2):
-	math.sqrt(distance_sq(point1, point2))
+    math.sqrt(distance_sq(point1, point2))
 
 def pt_in_rect(point, rect):
     (x, y) = point
@@ -36,5 +36,10 @@ def pt_in_rect(point, rect):
 
     return True
 
+def draw_collision_box():
+    for object in gfw.world.all_objects():
+        if hasattr(object, 'get_bb'):
+            draw_rectangle(*object.get_bb())
+
 if __name__ == "__main__":
-	print("This file is not supposed to be executed directly.")
+    print("This file is not supposed to be executed directly.")

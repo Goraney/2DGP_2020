9b498531084817e0751f1c1171a923c9ce860cd5
Goraney <btok92@gmail.com>
2020-12-07 07:10:50 +0900

    좌우 위치 체크/피격 후 1.5초 무적 판정 타이머

--- final_project/gobj.py
+++ final_project/gobj.py
@@ -1,10 +1,14 @@
 from pico2d import *
 import gfw
+import threading
 
-def point_add(point1, point2):
+count = 0
+
+def point_check(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
-    return x1+x2, y1+y2
+    if x1 < x2: return True
+    elif x1 >= x2: return False
 
 def collides_box(a, b):
     (la, ba, ra, ta) = a.get_bb()
@@ -23,7 +27,9 @@ def distance_sq(point1, point2):
     return (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 def distance(point1, point2):
-    math.sqrt(distance_sq(point1, point2))
+    if math.sqrt(distance_sq(point1, point2)) > 48: return False
+
+    return True
 
 def pt_in_rect(point, rect):
     (x, y) = point
@@ -40,6 +46,15 @@ def draw_collision_box():
     for obj in gfw.world.all_objects():
         if hasattr(obj, 'get_bb'):
             draw_rectangle(*obj.get_bb())
+
+def hit_timer():
+    global count
+    timer = threading.Timer(0.5, hit_timer)
+    timer.start()
+    count += 0.5
+    if count > 1.5:
+        timer.cancel()
+
 
 if __name__ == "__main__":
     print("This file is not supposed to be executed directly.")
